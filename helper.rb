814359f22e21fc6309a9538af58908414e0ff1be
vatogato <developer@vatogato.com>
2023-12-01 03:51:02 -0800

    checking out new helper session

--- helper.rb
+++ helper.rb
@@ -11,21 +11,17 @@ require_relative 'globals'
 #different scripts for tmux setup and pry
 
 
-step = ARGV[0]
-name = ARGV[1]
+name = ARGV[0]
 
 
 #todo: proper config constants for things like HELPER_COMMAND
-if step == "setup"
     puts "saving and creating new branch"
     git_command = "git add .; git commit -m 'checking out new helper session';" + "git checkout -B session_#{name}_#{Time.now.to_i}"
 
     Open3.capture2(git_command)
 
     puts "creating tmux session"
-    tmux_session = TmuxSession.create(name, detached:false, script:"ruby helper.rb start #{name}")
-
-elsif step == "start"
+    tmux_session = TmuxSession.create(name, detached:false,script:'')
 
     puts "changing directory to sessions"
     system("cd #{LIB_DIR}/sessions")
@@ -38,6 +34,5 @@ elsif step == "start"
     #refused, I say.  Whether or not this was a good idea, time will tell, but good and bad
     # are mostly subjective anyway, so whatever.
 
-end
 
 
