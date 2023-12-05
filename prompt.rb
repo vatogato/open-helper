4d697b2e74025885e4a0f25386f5f4fe73e4449e
vatogato <developer@vatogato.com>
2023-12-05 14:10:21 -0800

    changed prompt and removed type for now

--- prompt.rb
+++ prompt.rb
@@ -7,31 +7,31 @@ class Prompt
 #todo: proper encapsulation into type system
 #modes always includes the default
 #prompts dir structure is root/data/prompts/<type>/name_mode
-@@PROMPTS = [{type: 'system', name:'main', modes:['default','polite']}]
+@@PROMPTS = [{name:'main', modes:['default','polite']}]
 
-@@PROMPTS_DIR_PATH="./data/prompts/"
+@@PROMPTS_DIR_PATH="./"
 
 attr_reader :type, :name
 attr_accessor :mode
 
-def initialize(type, name, mode = 'default', variables = {})
+def initialize(name, type:nil, mode:'default', variables:{})
   @type = type
-  @name = name
+  @name = "prompt_#{name}"
   @mode = mode
   @variables = variables
 end
 
 def text
-  Prompt.text(@type, @name, @mode)
+  Prompt.text(@name, @mode)
 end
 
 def modes
-  Prompt.get_modes(@type, @name)
+  Prompt.get_modes(@name)
 end
 
 
-def self.get_modes(type, name)
-  @@PROMPTS.select{|obj| obj[:type] == type && obj[:name] == name }.first[:modes]
+def self.get_modes(name)
+  @@PROMPTS.select{|obj| obj[:name] == name }.first[:modes]
 end
 
 def self.text(type = nil, name = nil, mode = 'default')
@@ -40,14 +40,14 @@ def self.text(type = nil, name = nil, mode = 'default')
     raise ArgumentError, "Parameter #{param} is missing or undefined" unless binding.local_variable_defined?(param)
   end
 
-  get_prompt_text(get_prompt_filenames(type, name, mode))
+  get_prompt_text(get_prompt_filenames(name, mode))
 end
 
 def self.get_prompt_filenames(type = nil,name = nil,mode = nil)
   #hey look at me i learned a thing.  will try and do this or abstract to 
   # a validator module or something
   unless mode.nil?
-    return "#{@@PROMPTS_DIR_PATH}/#{type}/#{name}_#{mode}.txt"
+    return "#{@@PROMPTS_DIR_PATH}/#{name}_#{mode}.txt"
   end
 
   method_params = method(:get_prompt_filenames).parameters.map(&:last)
@@ -58,7 +58,7 @@ def self.get_prompt_filenames(type = nil,name = nil,mode = nil)
   filenames = []
     @@PROMPTS.select{|obj| obj[:type] == type && obj[:name] == name }.each do |prompt|
         prompt[:modes].each do |mode|
-            filenames << "#{prompt[:type]}_#{prompt[:name]}_#{mode}.txt"
+            filenames << "#{prompt[:name]}_#{mode}.txt"
         end
     end
     filenames
